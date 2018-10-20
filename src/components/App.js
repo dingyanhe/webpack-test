7cdea23f46d437fae1fa43d30643b27749067d2b
dingyanhe <dingyanhe@kezaihui.com>
2018-10-20 10:07:01 +0800

    update(full): first commit

---
+++ src/components/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+  NavLink,
+} from 'react-router-dom'
+
+
+import '@/styles/reset.styl'
+import '@/styles/App.styl'
+import logo from '@/assets/images/logo.png'
+
+import Recommend from './recommend/recommend'
+import Rank from './rank/rank'
+import Search from './search/search'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <header className="app-header">
+          <img src={logo} className="app-logo" alt="logo" />
+          <h1 className="app-title">Mango Music</h1>
+        </header>
+        <div className="music-tab">
+          <div className="tab-item">
+              <NavLink to="/recommend" className="nav-link">
+                  <span>推荐</span>
+              </NavLink>
+          </div>
+          <div className="tab-item">
+              <NavLink to="/ranking" className="nav-link">
+                  <span>排行榜</span>
+              </NavLink>
+          </div>
+          <div className="tab-item">
+              <NavLink to="/search" className="nav-link">
+                  <span>搜索</span>
+              </NavLink>
+          </div>
+        </div>
+        <div className="music-view">
+          {/*
+            Switch组件用来选择最近的一个路由，否则最后一个没有指定path的路由也会显示
+            Redirect重定向到列表页
+          */}
+          <Switch>
+            <Route path="/recommend" component={Recommend} />
+            <Route path="/rank" component={Rank} />
+            <Route path="/search" component={Search} />
+            <Redirect from="/" to="/recommend" />
+            <Route component={Recommend} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/rank/rank.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './rank.styl'
+
+export default class Rank extends React.Component {
+  render() {
+    return (
+        <div className="music-rank">
+            Rank
+        </div>
+    )
+  }
+}
+
